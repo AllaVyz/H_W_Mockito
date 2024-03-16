5c2abc54ebbbb7259ce983c821dc77c6ff8f3edb
AllaVyz <efialka@mail.ru>
2024-03-16 23:40:16 +0300

    Fix Test

--- src/test/java/ru/netology/statistic/KinopoiskTest.java
+++ src/test/java/ru/netology/statistic/KinopoiskTest.java
@@ -3,91 +3,113 @@ package ru.netology.statistic;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
- class KinopoiskTest {
+ public class KinopoiskTest {
 
-    @Test
-    public void testFindAll() {
-        Kinopoisk manager = new Kinopoisk();
-        String movie1 = "Звезда";
-        String movie2 = "Луна";
-        String movie3 = "Небо";
+     @Test
+     public void testFindAll() {
+         Kinopoisk manager = new Kinopoisk();
+         String movie1 = "Звезда";
+         String movie2 = "Луна";
+         String movie3 = "Небо";
 
-        manager.add(movie1);
-        manager.add(movie2);
-        manager.add(movie3);
-        String[] actual = manager.findAll();
-        String[] expected = {movie1, movie2, movie3};
+         manager.add(movie1);
+         manager.add(movie2);
+         manager.add(movie3);
+         String[] actual = manager.findAll();
+         String[] expected = {movie1, movie2, movie3};
 
-        Assertions.assertArrayEquals(expected, actual);
+         Assertions.assertArrayEquals(expected, actual);
 
-    }
+     }
 
-    @Test
-    public void testFindAll2() {
-        Kinopoisk manager = new Kinopoisk(12);
-        String movie1 = "Звезда";
-        String movie2 = "Луна";
-        String movie3 = "Небо";
+     @Test
+     public void testFindLastWhenLimitLessThanSize() {
+         Kinopoisk manager = new Kinopoisk();
+         String movie1 = "Звезда";
+         String movie2 = "Луна";
+         String movie3 = "Небо";
+         String movie4 = "Солнце";
+         String movie5 = "Земля";
+         String movie6 = "Космос";
 
-        manager.add(movie1);
-        manager.add(movie2);
-        manager.add(movie3);
-        String[] actual = manager.findAll();
-        String[] expected = {movie3, movie2, movie1};
+         manager.add(movie1);
+         manager.add(movie2);
+         manager.add(movie3);
+         manager.add(movie4);
+         manager.add(movie5);
+         manager.add(movie6);
 
-        Assertions.assertArrayEquals(expected, actual);
+         String[] actual = manager.findLast();
+         String[] expected = {"Космос", "Земля", "Солнце", "Небо", "Луна"};
 
-    }
+         Assertions.assertArrayEquals(expected, actual);
 
-    @Test
-    public void testFindAll3() {
-        Kinopoisk manager = new Kinopoisk();
-        String movie1 = "Звезда";
-        String movie2 = "Луна";
-        String movie3 = "Небо";
-        String movie4 = "Солнце";
-        String movie5 = "Земля";
-        String movie6 = "Космос";
-        String movie7 = "Марс";
+     }
 
-        manager.add(movie1);
-        manager.add(movie2);
-        manager.add(movie3);
-        manager.add(movie4);
-        manager.add(movie5);
-        manager.add(movie6);
-        manager.add(movie7);
-        String[] actual = manager.findAll();
-        String[] expected = {movie7, movie6, movie5, movie4, movie3};
+     @Test
+     public void testFindLastWhenLimitLessThanSize2() {
+         Kinopoisk manager = new Kinopoisk(3);
+         String movie1 = "Звезда";
+         String movie2 = "Луна";
+         String movie3 = "Небо";
+         String movie4 = "Солнце";
+         String movie5 = "Земля";
+         String movie6 = "Космос";
 
-        Assertions.assertArrayEquals(expected, actual);
+         manager.add(movie1);
+         manager.add(movie2);
+         manager.add(movie3);
+         manager.add(movie4);
+         manager.add(movie5);
+         manager.add(movie6);
 
-    }
+         String[] actual = manager.findLast();
+         String[] expected = {"Космос", "Земля", "Солнце"};
 
-    @Test
-    public void testFindAll4() {
-        Kinopoisk manager = new Kinopoisk(6);
-        String movie1 = "Звезда";
-        String movie2 = "Луна";
-        String movie3 = "Небо";
-        String movie4 = "Солнце";
-        String movie5 = "Земля";
-        String movie6 = "Космос";
-        String movie7 = "Марс";
+         Assertions.assertArrayEquals(expected, actual);
 
-        manager.add(movie1);
-        manager.add(movie2);
-        manager.add(movie3);
-        manager.add(movie4);
-        manager.add(movie5);
-        manager.add(movie6);
-        manager.add(movie7);
-        String[] actual = manager.findAll();
-        String[] expected = {movie7, movie6, movie5, movie4, movie3, movie2};
+     }
+     @Test
+     public void testFindLastWhenLimitMoreThanSize() {
+         Kinopoisk manager = new Kinopoisk();
+         String movie1 = "Звезда";
+         String movie2 = "Луна";
+         String movie3 = "Небо";
 
-        Assertions.assertArrayEquals(expected, actual);
-    }
-}
+
+         manager.add(movie1);
+         manager.add(movie2);
+         manager.add(movie3);
+
+
+         String[] actual = manager.findLast();
+         String[] expected = {"Небо", "Луна", "Звезда"};
+
+         Assertions.assertArrayEquals(expected, actual);
+
+     }
+     @Test
+     public void testFindLastWhenLimitMoreThanSize2() {
+         Kinopoisk manager = new Kinopoisk(4);
+         String movie1 = "Звезда";
+         String movie2 = "Луна";
+         String movie3 = "Небо";
+
+
+         manager.add(movie1);
+         manager.add(movie2);
+         manager.add(movie3);
+
+
+         String[] actual = manager.findLast();
+         String[] expected = {"Небо", "Луна", "Звезда"};
+
+         Assertions.assertArrayEquals(expected, actual);
+
+     }
+ }
+
+
 
 
 
